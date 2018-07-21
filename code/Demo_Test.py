de50408c3600de0b523ada870ff1dbefe920e227
张炎 <970730254@qq.com>
2018-07-21 12:46:33 +0800

    notebook

---
+++ code/Demo_Test.py
@@ -0,0 +1,22 @@
+# _*_ coding: utf-8 _*_
+
+__author__ = 'Morgan'
+
+'''
+python spider -- OpenCV | | Demo2
+'''
+import cv2
+import sys
+
+# main()
+if __name__ == "__main__":
+    if len(sys.argv[1]) > 1:
+        # input the image
+        image = cv2.imread(sys.argv[1], cv2.IMREAD_ANYCOLOR)
+    else:
+        print("Usge: python Demo_Test.py imageFile")
+
+    # display the picture
+    cv2.imshow('image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
---
+++ code/image_digitization.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Morgan'
+
+'''
+python opencv | image digitization
+'''
+import numpy as np
+
+# (1) 构建对象
+
